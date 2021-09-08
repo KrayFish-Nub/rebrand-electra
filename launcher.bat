b6e6931bfbdc5b3e204cc90246a2631b9bd40813
Konrad570 <Konradtoesch@icloud.com>
2021-09-08 22:25:54 +0200

    Nodemon & Launcher Fix

--- launcher.bat
+++ launcher.bat
@@ -5,10 +5,9 @@ cls
 Echo ElectraV2 DevOps - Panel
 Echo ============================Electra Panel============================
 Echo 1 Install Modules           Installs Required modules to load app!  (AFTER INSTALL RELAUNCH APP)          
-Echo 2 Download NODEJS           Downloads NODEJS (Windows/Default Browser)
-Echo 3 Start Node App            Launches Gen on [PORT Specified] - DC API
-Echo 4 Nodemon Setup             Required downloads of Nodemon (Server)
-Echo 5 Exit Launcher/Close       Closes this menu
+Echo 2 Start Node App            Launches Gen on [PORT Specified] - DC API
+Echo 3 Nodemon Setup             Required downloads of Nodemon (Server)
+Echo 4 Exit Launcher/Close       Closes this menu
 Echo =====================================================================
 Choice /C 1234 /M "Select a choice below "
 
@@ -21,21 +20,16 @@ If Errorlevel 1 Goto 1
 Goto End
 
 
-:5
+:4
 :End
 
-:4
+:3
 npm i nodemon
 pause
 :End
 
-:3
-node .
-pause
-:End
-
 :2
-start   https://nodejs.org/dist/v16.9.0/node-v16.9.0-x64.msi
+node .
 pause
 :End
 
