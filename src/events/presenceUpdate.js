6eea2d50a886575ad14665567c32a8b1566af809
ilias-hhu <aoskenshin@gmail.com>
2021-09-12 01:49:14 +0200

    added error handling in events.

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -3,6 +3,8 @@
 const { PresenceUpdateStatus } = require("discord-api-types/v9");
 const { Presence } = require("discord.js");
 const Guild = require("../models/guilds.js");
+const { red } = require("colors/safe");
+
 module.exports.data = {
     name: "presenceUpdate",
     once: false,
@@ -16,45 +18,50 @@ module.exports.data = {
 
 module.exports.run = async (oldPresence, newPresence) =>
 {
-    if (!oldPresence || !oldPresence.user.bot) return;
-
-    if (oldPresence.status == newPresence.status) return;
-
-    const guildQuery = await Guild.findOne({ id: oldPresence.guild.id });
-    if (guildQuery)
+    try
     {
-        /* Bot went online. */
-        if (newPresence.status == PresenceUpdateStatus.Online)
-        {
-            const channel = await newPresence.guild.channels.fetch(guildQuery.channel);
-            channel.send({
-                content: `<@&${guildQuery.role}>`,
-                embeds: [{
-                    title: "Bot went online!",
-                    description: `Looks like ${newPresence.member.displayName} (${newPresence.user.tag}) just went online!`,
-                    thumbnail: {
-                        url: newPresence.user.avatarURL({ format: "png", size: 1024 }),
-                    },
-                    timestamp: new Date()
-                }]
-            });
-        }
+        if (!oldPresence || !oldPresence.user.bot) return;
 
-        /* Bot went offline. */
-        else if (newPresence.status == PresenceUpdateStatus.Offline || newPresence.status == PresenceUpdateStatus.Invisible)
+        if (oldPresence.status == newPresence.status) return;
+
+        const guildQuery = await Guild.findOne({ id: oldPresence.guild.id });
+        if (guildQuery)
         {
-            const channel = await newPresence.guild.channels.fetch(guildQuery.channel);
-            channel.send({
-                content: `<@&${guildQuery.role}>`,
-                embeds: [{
-                    title: "Bot went offline!",
-                    description: `Looks like ${newPresence.member.displayName} (${newPresence.user.tag}) just went offline!`,
-                    thumbnail: {
-                        url: newPresence.user.avatarURL({ format: "png", size: 1024 }),
-                    },
-                    timestamp: new Date()
-                }]
-            });
+            /* Bot went online. */
+            if (newPresence.status == PresenceUpdateStatus.Online)
+            {
+                newPresence.guild.channels.cache.get(guildQuery.channel)?.send({
+                    content: `<@&${guildQuery.role}>`,
+                    embeds: [{
+                        title: "Bot went online!",
+                        description: `Looks like ${newPresence.member.displayName} (${newPresence.user.tag}) just went online!`,
+                        thumbnail: {
+                            url: newPresence.user.avatarURL({ format: "png", size: 1024 }),
+                        },
+                        timestamp: new Date()
+                    }]
+                });
+            }
+
+            /* Bot went offline. */
+            else if (newPresence.status == PresenceUpdateStatus.Offline || newPresence.status == PresenceUpdateStatus.Invisible)
+            {
+                newPresence.guild.channels.cache.get(guildQuery.channel)?.send({
+                    content: `<@&${guildQuery.role}>`,
+                    embeds: [{
+                        title: "Bot went offline!",
+                        description: `Looks like ${newPresence.member.displayName} (${newPresence.user.tag}) just went offline!`,
+                        thumbnail: {
+                            url: newPresence.user.avatarURL({ format: "png", size: 1024 }),
+                        },
+                        timestamp: new Date()
+                    }]
+                });
+            }
         }
+    }
+    catch (err)
+    {
+        console.error(red(err));
     }
 };
