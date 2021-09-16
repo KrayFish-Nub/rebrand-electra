36a323976eec2e7728840debaadcf4b4b4593589
ilias-hhu <aoskenshin@gmail.com>
2021-09-16 15:54:10 +0200

    added new uptime command.

--- src/util/util.js
+++ src/util/util.js
@@ -6,6 +6,13 @@ const path = require("path");
 /* Various utility functions. */
 
 /**
+ * Storing the bots color plaette
+ */
+module.exports.colorPalette = {
+    brandingColor: 475478
+};
+
+/**
  * Recursively read all files in a directory.
  * @param {fs.PathLike} dirPath The path to the directory that will be recursively traversed. 
  * @param {Array} arrayOfFiles The array that all files will be recursively pushed to.
@@ -39,7 +46,7 @@ module.exports.getKeyByValue = function (object, value)
 
 /**
  * Formats milliseconds to minutes and seconds. 298999 -> 4:59
- * @param {Number} ms The amount of milliseconds.
+ * @param {Number} ms The amount of milliseconds to format.
  * @returns Returns the formatted string.
  */
 module.exports.msToMinAndSec = function (ms)
@@ -47,5 +54,20 @@ module.exports.msToMinAndSec = function (ms)
     var minutes = Math.floor(ms / 60000);
     var seconds = ((ms % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
+};
+
+/**
+ * Formats the ms to days, hours, minutes and seconds and returns those values in an object.
+ * @param {Number} ms The amount of milliseconds to format.
+ */
+module.exports.msToTimeObj = function (ms)
+{
+    return {
+        days: Math.floor(ms / 86400000),
+        hours: Math.floor(ms / 3600000) % 24,
+        minutes: Math.floor(ms / 60000) % 60,
+        seconds: Math.floor(ms / 1000) % 60
+    };
+};
+
 
