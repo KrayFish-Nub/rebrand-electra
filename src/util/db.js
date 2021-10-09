8e32653fc4f18aa80414a2ef7dcda6176ffdc14c
Konrad570 <Konradtoesch@icloud.com>
2021-10-09 22:24:41 +0200

    added env for database key

--- src/util/db.js
+++ src/util/db.js
@@ -7,7 +7,8 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 console.log(cyan("Connecting to Database . . ."));
-connect("mongodb+srv://Koni:KoniKlaraManu22@cluster0.7losc.mongodb.net/ElectraBot?authSource=admin&replicaSet=atlas-5d6uzz-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true").then(() =>
+connect(process.env.MONGO_TOKEN).then(() =>
+
 {
     console.log(green("Successfully connected to database."));
 }).catch((err) => console.error(red(err)));
